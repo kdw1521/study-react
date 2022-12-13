0d52139383808bd0bd9bdd4edce64168e402e46a
김도완 <wando@gimdowan-ui-MacBookPro.local>
2022-12-14 08:55:07 +0900

    feat: study styled-components

--- src/context/Main.jsx
+++ src/context/Main.jsx
@@ -1,18 +1,23 @@
 import React, { useContext } from "react";
 import ThemeContext from "./ThemeContext";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    background: #f1f1f1;
+    color: ${props => props.theme === 'light' ? 'blue' : 'red'};
+    display: flex;
+    justify-content: space-around;
+`
 
 function Main(props) {
 
     const [theme, toggleTheme] = useContext(ThemeContext);
 
     return (
-        <div style={{
-            backgroundColor: theme === "light" ? "white" : "#000000",
-            color: theme === "light" ? "#000000" : "white"
-        }}>
+        <Wrapper theme={theme}>
             <h3>Main</h3>
             <button onClick={toggleTheme}>Toggle Theme</button>
-        </div>
+        </Wrapper>
     )
 }
 
