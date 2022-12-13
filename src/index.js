433b86e42eead2b7986b994909d0a78947140d08
김도완 <wando@gimdowan-ui-MacBookPro.local>
2022-12-13 09:01:40 +0900

    feat: study component & props

--- src/index.js
+++ src/index.js
@@ -5,25 +5,27 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Course from './jsx/Course';
 import Time from './element/Time';
+import MovieList from './movies/MovieList';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     {/* <App /> */}
-//     <Course />
-//   </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    {/* <Course /> */}
+    <MovieList />
+  </React.StrictMode>
+);
 
-setInterval(() => {
-  // ReactDOM.render is no longer supported in React 18. Use createRoot instead. Until you switch to the new API, your app will behave as if it's running React 17
-  // ReactDom render 는 18버전부터는 지원하지 않아서 17 이하 버전에서 사용해야함.
-  ReactDOM.render( 
-    <React.StrictMode>
-      <Time />
-    </React.StrictMode>,
-    document.getElementById('root')
-  )
-}, 1000);
+// setInterval(() => {
+//   // ReactDOM.render is no longer supported in React 18. Use createRoot instead. Until you switch to the new API, your app will behave as if it's running React 17
+//   // ReactDom render 는 18버전부터는 지원하지 않아서 17 이하 버전에서 사용해야함.
+//   ReactDOM.render( 
+//     <React.StrictMode>
+//       <Time />
+//     </React.StrictMode>,
+//     document.getElementById('root')
+//   )
+// }, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
